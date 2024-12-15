77240312f9111b9abc0c04969e23586c0dc90ee8
Daniel Garza <dgarz160@stu.southtexascollege.edu>
2024-12-14 19:26:35 -0600

    404 changes

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
-//import { createRouter, createWebHistory } from 'vue-router'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
+//import { createRouter, createWebHashHistory } from 'vue-router'
 
 import HomePage from '@/views/HomePage.vue'
 import MealsByName from './views/MealsByName.vue'
@@ -9,18 +9,20 @@ import MealDetails from './views/MealDetails.vue'
 import Ingredients from './views/Ingredients.vue'
 
 const routes = [
+  //{ path: '/meal-search/', name: 'Home', component: HomePage },
   { path: '/meal-search/', name: 'Home', component: HomePage },
   { path: '/meal-search/by-name/:name?', name: 'MealsByName', component: MealsByName },
   { path: '/meal-search/by-letter/:letter?', name: 'MealsByLetter', component: MealsByLetter },
   { path: '/meal-search/by-ingredient/:ingredient?', name: 'MealsByIngredients', component: MealsByIngredients },
   { path: '/meal-search/meal/:id', name: 'MealDetails', component: MealDetails },
   { path: '/meal-search/ingredients', name: 'Ingredients', component: Ingredients },
-  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/meal-search/other', name: 'Other', component: () => import('@/views/OtherPage.vue') }
+  
 ]
 
 const router = createRouter({
-  //  history: createWebHistory(),
-  history: createWebHashHistory(),
+    history: createWebHistory(),
+  //history: createWebHashHistory(),
   routes,
 })
 
